21c3a996ddd163b4c792650e7ec64870bf91fcd6
tbuchanan <tbuchanan@overstock.com>
2019-08-23 17:48:15 -0600

    More stuff..

--- src/test/java/ui/FindFlightsUITest.java
+++ src/test/java/ui/FindFlightsUITest.java
@@ -7,15 +7,25 @@ import net.thucydides.core.annotations.WithTag;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import steps.ui.FindFlightsUISteps;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RunWith(SerenityRunner.class)
 public class FindFlightsUITest {
+    ChromeOptions chromeOptions = new ChromeOptions();
+
     @Managed
-    WebDriver driver;
+    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
 
     @Steps
     FindFlightsUISteps findFlightsUISteps;
+
+    public FindFlightsUITest() throws MalformedURLException {
+    }
 
     @Test
     @WithTag(type="type", name="UI")
