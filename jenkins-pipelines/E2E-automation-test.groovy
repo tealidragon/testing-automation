8e23c7c7848d6d19ab69c33a232ea5133a80dbfb
Tilli Buchanan <tbuchanan@overstock.com>
2019-08-29 10:11:58 -0600

    CLUBO-6649  - Pipeline Automation  - Jenkins file (attempting to set to checkout of git repo from ostk)

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -1,48 +1,72 @@
-#!groovy
-node {
-    stage('Git checkout') { // for display purposes
-        git 'https://github.com/BushnevYuri/e2e-automation-pipeline.git'
-    }
-    stage('Smoke') {
-        try {
-            sh "mvn clean verify -Dtags='type:Smoke'"
-        } catch (err) {
+#!/usr/bin/env/groovy
+@Library('ostk-pipeline')
+import com.overstock.dx.Pipeline
+@Library('ostk-pipeline')
+import com.overstock.dx.Pipeline
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "Smoke tests report"
-            ])
-        }
-    }
-    stage('API') {
-        try {
-            sh "mvn clean verify -Dtags='type:API'"
-        } catch (err) {
+def areDeploying = env.BRANCH_NAME == 'master'
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "API tests report"
-            ])
+new Pipeline(this).withMaven('3.5.0').withJava('8').execute {
+    installWebhooks(scm)
+    node {
+        stage('Git checkout') { // for display purposes
+            // Checkout code, cleaning the tree first if necessary
+            ostkCheckout()
         }
-    }
-    stage('UI') {
-        try {
-            sh "mvn clean verify -Dtags='type:UI'"
-        } catch (err) {
+        stage('Smoke') {
+            try {
+                sh "mvn clean verify -Dtags='type:Smoke'"
+            } catch (err) {
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "UI tests report"
-            ])
+            } finally {
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "Smoke tests report"
+                ])
+            }
         }
-    }
-    stage('Results') {
-        junit '**/target/failsafe-reports/*.xml'
+        stage('Db') {
+            try {
+                sh "mvn clean verify -Dtags='type:DB'"
+            } catch (err) {
+
+            } finally {
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "DB tests report"
+                ])
+            }
+        }
+        stage('API') {
+            try {
+                sh "mvn clean verify -Dtags='type:API'"
+            } catch (err) {
+
+            } finally {
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "API tests report"
+                ])
+            }
+        }
+        stage('UI') {
+            try {
+                sh "mvn clean verify -Dtags='type:UI'"
+            } catch (err) {
+
+            } finally {
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "UI tests report"
+                ])
+            }
+        }
+        stage('Results') {
+            junit '**/target/failsafe-reports/*.xml'
+        }
     }
 }
