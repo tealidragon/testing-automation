70af3cc1ce048a0de18dc1fe18635ccc293eeec3
Tilli Buchanan <tbuchanan@overstock.com>
2019-08-29 10:22:34 -0600

    CLUBO-6649  - Removing UI from Jenkins file as that is not yet implemented.

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -52,19 +52,6 @@ new Pipeline(this).withMaven('3.5.0').withJava('8').execute {
                 ])
             }
         }
-        stage('UI') {
-            try {
-                sh "mvn clean verify -Dtags='type:UI'"
-            } catch (err) {
-
-            } finally {
-                publishHTML(target: [
-                        reportDir  : 'target/site/serenity',
-                        reportFiles: 'index.html',
-                        reportName : "UI tests report"
-                ])
-            }
-        }
         stage('Results') {
             junit '**/target/failsafe-reports/*.xml'
         }
