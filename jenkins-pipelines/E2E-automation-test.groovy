21d4dc3faa551be7ee2dd86e856d04f5fcc86356
Tilli Buchanan <tbuchanan@overstock.com>
2019-09-12 08:48:04 -0600

    Updating groovy pipeline script to run UI tests.

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -47,6 +47,19 @@ node {
             ])
         }
     }
+    stage('UI') {
+        try {
+            sh "mvn clean verify -Dtags='type:UI'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "UI tests report"
+            ])
+        }
+    }
     stage('Results') {
         junit '**/target/failsafe-reports/*.xml'
     }
