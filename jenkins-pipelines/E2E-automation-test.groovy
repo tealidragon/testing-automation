e50b2a9455bfef6ae6c11d0bcb644366b35633a9
Tilli Buchanan <tbuchanan@overstock.com>
2019-09-24 15:50:13 -0600

    Adding archiveArtifacts artifacts: 'build/libs/**/*.jar', fingerprint: true to remove error: Recording test results
                                                                                                 Test reports were found but none of them are new. Did leafNodes run?

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -63,7 +63,7 @@ node {
     }
     stage('Results') {
         archiveArtifacts artifacts: '**/target/failsafe-reports/*.xml', fingerprint: true
-        sh '*.xml'
+        sh 'touch target/failsafe-reports/*.xml'
         junit '**/target/failsafe-reports/*.xml'
     }
 }
