6675476fa03d06f7ca6201f8e275abfe044d6b5c
Tilli Buchanan <tbuchanan@overstock.com>
2019-10-08 15:51:02 -0600

    - Pushing jenkins file.

--- jenkins-pipelines/serenity-ui-api-automated-testing.groovy
+++ jenkins-pipelines/serenity-ui-api-automated-testing.groovy
@@ -13,89 +13,40 @@ node {
             sh "mvn clean verify -Dtags='type:Smoke'"
         } catch (err) {
 
-        } finally {
-
-            publishHTML(target: [
-                    reportDir  : 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName : 'Smoke tests report',
-                    keepAll:     true,
-                    alwaysLinkToLastBuild: true,
-                    allowMissing: false
-            ])
         }
     }
     stage('Db') {
         try {
             sh "mvn clean verify -Dtags='type:DB'"
-        } catch (err) {
+        } catch (err)
 
-        } finally {
-            publishHTML(target: [
-                    reportDir  : 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName : "DB tests report",
-                    keepAll:     true,
-                    alwaysLinkToLastBuild: true,
-                    allowMissing: false
-            ])
-        }
     }
     stage('API') {
         try {
             sh "mvn clean verify -Dtags='type:API'"
-        } catch (err) {
+        } catch (err)
 
-        } finally {
-            publishHTML(target: [
-                    reportDir  : 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName : "API tests report",
-                    keepAll:     true,
-                    alwaysLinkToLastBuild: true,
-                    allowMissing: false
-            ])
-        }
     }
     stage('PeopleAPI') {
         try {
             sh "mvn clean verify -Dtags='type:PeopleAPI'"
-        } catch (err) {
+        } catch (err)
 
-        } finally {
-            publishHTML(target: [
-                    reportDir  : 'target/site/ serenity',
-                    reportFiles: 'index.html',
-                    reportName : "API tests report",
-                    keepAll:     true,
-                    alwaysLinkToLastBuild: true,
-                    allowMissing: false
-            ])
-        }
     }
     stage('UI') {
         try {
             sh "mvn clean verify -Dtags='type:UI'"
-        } catch (err) {
+        } catch (err)
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/ serenity',
-                    reportFiles: 'index.html',
-                    reportName: "UI tests report",
-                    keepAll:     true,
-                    alwaysLinkToLastBuild: true,
-                    allowMissing: false
-            ])
-        }
     }
     stage('Publish Results') {
-        sh "cd ${serenityTargetBasePath}/target/site/serenity && find * -type f -exec curl -u daemon:xampp --ftp-create-dirs -T {} ftp://${serenityReportServer}/ftpUploads/${serenityAppFolderName}/serenity/{} \\;"
-        sh "curl -X GET http://${serenityReportServer}:8080/archive/${serenityAppFolderName}/serenity"
-        echo "Link to live report: http://${serenityReportServer}:8080/reports/${serenityAppFolderName}/live/index.html"
-        currentBuild.description = "Click <a href='http://${serenityReportServer}:8080/${serenityAppFolderName}/live'>here</a> for Serenity Reports"
-        sh "cd ../../../../"
-
+        publishHTML(target: [
+                reportDir  : './target/site/serenity',
+                reportFiles: 'index.html',
+                keepAll:     true,
+                alwaysLinkToLastBuild: true,
+                allowMissing: false
+        ])
         junit '**/target/failsafe-reports/*.xml'
     }
 }
